6285e8a150ed5d90ab482f97010c9cd3758e0add
Ayrat Nazmutdinov <nazmut.ayr.97@mail.ru>
2022-01-28 11:48:46 +0300

    Tasks by function

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,61 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = Infinity;
+  let max = Infinity;
+  let sum = 0;
+  let avg = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+    }
+    avg = Number(sum / arr.length).toFixed(2);
+    return { min: min, max: max, avg: avg };
+  }
+  getArrayParams([-99, 99, 10])
 
-  return { min: min, max: max, avg: avg };
-}
+  //// Задание 2
+  function worker(arr) {
+  let sum = 0;
 
-// Задание 2
-function worker(arr) {
-  let sum;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
 
-  // Ваш код
+  const w = worker;
 
-  return sum;
-}
+  function makeWork(arrOfArr, func) {    
+  let max = -Infinity;
 
-function makeWork(arrOfArr, func) {
-  let max;
+    for (let i = 0; i < arrOfArr.length; i++) {
+      if (func(arrOfArr[i]) > max) {
+        max = func(arrOfArr[i])
+      }
+    }
+    return max;
+  }
+  arrOfArr = [[1, 2, 3, 4], [10, 20, -10, -20]]
+  makeWork(arrOfArr, w)
 
-  // Ваш кода
-  // for ...
-  
-  return max;
-}
+  // Задание 3
+  function worker2(arr) {
+  let min = Infinity;
+  let max = -Infinity;
 
-// Задание 3
-function worker2(arr) {
-  // Ваш код
-}
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+    }
+      if (arr[i] < min) {
+        min = arr[i];
+      }
+  }
+  return Math.abs(max - min);
+  }
