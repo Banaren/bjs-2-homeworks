59c7d97e99200be423bea539c0047929d020aa03
Ayrat Nazmutdinov <nazmut.ayr.97@mail.ru>
2022-03-17 21:48:11 +0300

    Functions decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,42 @@
-function cachingDecoratorNew(func) {
-  // Ваш код
+function cachingDecoratorNew() {
+  const hash = {}
+
+  return function wrapper(...args) {
+
+    if (Object.keys(hash).length > 4) delete hash[Object.keys(hash)[0]];
+
+    if (hash[args]) return ('из кэша:' + hash[args]);
+
+    if (!hash[args]) {
+      hash[args] = [...args].reduce((acc, item) => acc + item);
+      return ('вычисляем:' + hash[args]);
+    }
+  }
+}
+
+function debounceDecoratorNew(f, ms) {
+  let isCooldown = false;
+
+  return function wrapper() {
+
+    if (isCooldown) return;
+    f.apply(this, null);
+    isCooldown = true;
+    setTimeout(() => isCooldown = false, ms);
+  };
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
+function debounceDecorator2(f, ms) {
+  let isCooldown = false;
+  let count = 1;
 
-function debounceDecorator2(func) {
-  // Ваш код
+  return function wrapper() {
+    console.log('Было произведено ' + count++ + ' вызов(а)(ов)');
+    
+    if (isCooldown) return;
+    f.apply(this, null);
+    isCooldown = true;
+    setTimeout(() => isCooldown = false, ms);
+  };
 }
